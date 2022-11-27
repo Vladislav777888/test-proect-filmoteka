0922cc28c2ec5ab118987db600a1b27f170ed8c1
Vladislav777888 <106475603+Vladislav777888@users.noreply.github.com>
2022-11-27 10:52:52 +0000

    Deploying to gh-pages from @ Vladislav777888/test-proect-filmoteka@d8e2d0a8c36a36a3c9c91a3e4850fccb53e9c04a 🚀

---
+++ index.2ca6abfc.js
@@ -0,0 +1,2 @@
+!function(){var e={home:document.querySelector(".js-link-home"),library:document.querySelector(".js-link-library"),form:document.querySelector(".js-header-form")};e.library.addEventListener("click",(function(r){r.preventDefault(),e.home.classList.remove("link--current"),r.target.classList.add("link--current"),e.form.style.display="none"}))}();
+//# sourceMappingURL=index.2ca6abfc.js.map
---
+++ index.d1fc271b.js
@@ -0,0 +1,2 @@
+const e={home:document.querySelector(".js-link-home"),library:document.querySelector(".js-link-library"),form:document.querySelector(".js-header-form")};e.library.addEventListener("click",(function(r){r.preventDefault(),e.home.classList.remove("link--current"),r.target.classList.add("link--current"),e.form.style.display="none"}));
+//# sourceMappingURL=index.d1fc271b.js.map
